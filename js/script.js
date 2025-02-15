7489a05e1a8c0f1f5e8c0c147e4facad9186cecc
nayebislam <nayebislamnafi475@gmail.com>
2025-02-16 01:32:29 +0600

    js update

--- js/script.js
+++ js/script.js
@@ -8,22 +8,22 @@ let bmiResult = document.getElementById("bmiResult")
 calculateBMI.addEventListener("click", function () {
     if (Weight.value == "" && Height.value == "") {
         bmiResult.innerHTML = "Please enter valid weight & height." 
-        bmiResult.className = "bmi_result text-center text-xl font-semibold p-6 rounded-xl calculator text-gray-600"
+        bmiResult.className = "bmi_result text-center text-xl font-semibold p-6 rounded-xl calculator text-red-600"
         bmiResult.classList.remove("hidden")
     } else {
-        let BMI = (Weight.value * 2.20462) / (Height.value * Height.value) * 703
+        let BMI = ((Weight.value * 2.20462) / (Height.value * Height.value) * 703).toFixed(2)
         let category = ""
         let colrclass = ""
-        if (BMI < 18.5) {
+        if (BMI <= 18.5) {
             category = "Underweight"
             colrclass = "text-blue-600"
-        } else if(BMI < 25) {
+        } else if(BMI <= 24.9) {
             category = "Normal weight"
             colrclass = "text-green-600"
-        } if (BMI < 30) {
+        } if (BMI >= 25) {
             category = "Overweight"
             colrclass = "text-yellow-600"
-        } else {
+        } else if (BMI >= 30) {
             category = "Obesity"
             colrclass = "text-red-600"
         }
