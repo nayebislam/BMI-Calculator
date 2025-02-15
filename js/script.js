234d3c534dd78db0cfa224ea5d2c9ce45019434e
nayebislam <nayebislamnafi475@gmail.com>
2025-02-16 01:17:10 +0600

    add all file

---
+++ js/script.js
@@ -0,0 +1,40 @@
+let Weight = document.getElementById("Weight")
+let Height = document.getElementById("Height")  
+let calculateBMI = document.getElementById("calculateBMI")
+let bmiResult = document.getElementById("bmiResult")
+
+
+
+calculateBMI.addEventListener("click", function () {
+    if (Weight.value == "" && Height.value == "") {
+        bmiResult.innerHTML = "Please enter valid weight & height." 
+        bmiResult.className = "bmi_result text-center text-xl font-semibold p-6 rounded-xl calculator text-gray-600"
+        bmiResult.classList.remove("hidden")
+    } else {
+        let BMI = (Weight.value * 2.20462) / (Height.value * Height.value) * 703
+        let category = ""
+        let colrclass = ""
+        if (BMI < 18.5) {
+            category = "Underweight"
+            colrclass = "text-blue-600"
+        } else if(BMI < 25) {
+            category = "Normal weight"
+            colrclass = "text-green-600"
+        } if (BMI < 30) {
+            category = "Overweight"
+            colrclass = "text-yellow-600"
+        } else {
+            category = "Obesity"
+            colrclass = "text-red-600"
+        }
+
+        bmiResult.innerHTML = `
+                          <div class="space-y-2">
+                            <div class="text-2xl font-bold ${colrclass}">BMI: ${BMI}</div>
+                            <div class="text-gray-600">${category}</div>
+                          </div>`
+        bmiResult.className = "bmi_result text-center text-xl font-semibold p-6 rounded-xl hidden calculator"
+        bmiResult.classList.remove("hidden")
+        // bmiResult.innerHTML = `${BMI}`
+    }
+})
